8c8f36929058dbb9a887afdf39b6ead88086fdc9
eliezerangel <eliezerangel@gmail.com>
2024-02-24 22:39:17 +0000

    added slider -home, alert sucess - contact

--- script.js
+++ script.js
@@ -17,36 +17,49 @@ function updateDateTime() {
 function getDateTime() {  
     let currentDate = new Date();      
     //let currentDate = currentTime.getVarDate(); 
-    document.querySelector('#datetime').textContent = currentDate;
+    document.querySelector('#datetime').textContent = currentDate; 
+    //console.log('HORAS');
 } 
   
 function openModal(){  
 
-
-
-
-
-
     const exampleModal = document.getElementById('exampleModal');
-
-    console.log(exampleModal)
-
+    //console.log(exampleModal)
 
     if (exampleModal) {
-    exampleModal.addEventListener('show.bs.modal', event => {
-        // Button that triggered the modal
-        const button = event.relatedTarget
-        // Extract info from data-bs-* attributes
-        const recipient = button.getAttribute('data-bs-whatever')
-        // If necessary, you could initiate an Ajax request here
-        // and then do the updating in a callback.
+        exampleModal.addEventListener('show.bs.modal', event => {
+            // Button that triggered the modal
+            const button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+            const recipient = button.getAttribute('data-bs-whatever')
+            // If necessary, you could initiate an Ajax request here
+            // and then do the updating in a callback.
+            // Update the modal's content.
+            const modalTitle = exampleModal.querySelector('.modal-title')
+            const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-        // Update the modal's content.
-        const modalTitle = exampleModal.querySelector('.modal-title')
-        const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-        modalTitle.textContent = `New message to ${recipient}`
-        modalBodyInput.value = recipient
-    })
+            modalTitle.textContent = `New message to ${recipient}`
+            modalBodyInput.value = recipient
+        })
     }
+}
+
+function recarregarPagina() {
+    setTimeout(function() {
+        location.reload(); // recarrega a página
+    }, 3000); // 2000 milissegundos = 2 segundos
+}
+
+function showMessage(event){    
+    event.preventDefault(); // Para evitar o envio do formulário
+    
+    var nome   = document.getElementById("name").value;
+    var alerta = document.getElementById("alert_success");
+    // Substitui o conteúdo usando innerHTML
+    alerta.innerHTML = "Olá "+nome+"! Sua mensagem foi enviada com sucesso.";
+
+    document.getElementById("alert_success").removeAttribute("hidden");
+    topFunction();
+    // Chama a função para recarregar a página após dois segundos
+    recarregarPagina(); 
 }
