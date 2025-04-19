8112856c09b55850b754aa3e6b2df20f7abc13c0
eliezerangel <eliezerangel@gmail.com>
2025-04-19 21:55:39 +0100

    about page suspended to update content

---
+++ update.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    setTimeout(function () {
+      window.location.href = "../index.html"; // Substitua pela URL desejada
+    }, 6000); // 3000 milissegundos = 3 segundos
+  });
