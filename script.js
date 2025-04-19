bfa17d6415a48e671e79400bc9df6398bd8c1dca
eliezerangel <eliezerangel@gmail.com>
2025-04-19 20:47:40 +0100

    comment some js code and removed modal attribute

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ function topFunction() {
 
 function updateDateTime() {  
     let currentTime = new Date();      
-    let currentHour   =  currentTime.getHours().toString().padStart(2, '0');
-    let currentMinute =  currentTime.getUTCMinutes().toString().padStart(2, '0');
-    let currentSecond =  currentTime.getSeconds().toString().padStart(2, '0');
-
-    //let hour = currentHour+':'+currentMinute+':'+currentSecond;   
-    //document.querySelector('#time').textContent = hour;
+    //let currentHour   =  currentTime.getHours().toString().padStart(2, '0');
+    //let currentMinute =  currentTime.getUTCMinutes().toString().padStart(2, '0');
+    //let currentSecond =  currentTime.getSeconds().toString().padStart(2, '0');
 } 
 
 
@@ -18,53 +15,51 @@ function getDateTime() {
     let currentDate = new Date();      
     //let currentDate = currentTime.getVarDate(); 
     document.querySelector('#datetime').textContent = currentDate; 
-    //console.log('HORAS');
 } 
   
-function openModal(){  
+// function openModal(){  
 
-    const exampleModal = document.getElementById('exampleModal');
-    //console.log(exampleModal)
+//     const exampleModal = document.getElementById('exampleModal');
+//     //console.log(exampleModal)
 
-    if (exampleModal) {
-        exampleModal.addEventListener('show.bs.modal', event => {
-            // Button that triggered the modal
-            const button = event.relatedTarget
-            // Extract info from data-bs-* attributes
-            const recipient = button.getAttribute('data-bs-whatever')
-            // If necessary, you could initiate an Ajax request here
-            // and then do the updating in a callback.
-            // Update the modal's content.
-            const modalTitle = exampleModal.querySelector('.modal-title')
-            const modalBodyInput = exampleModal.querySelector('.modal-body input')
+//     if (exampleModal) {
+//         exampleModal.addEventListener('show.bs.modal', event => {
+//             // Button that triggered the modal
+//             const button = event.relatedTarget
+//             // Extract info from data-bs-* attributes
+//             const recipient = button.getAttribute('data-bs-whatever')
+//             // If necessary, you could initiate an Ajax request here
+//             // and then do the updating in a callback.
+//             // Update the modal's content.
+//             const modalTitle = exampleModal.querySelector('.modal-title')
+//             const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-            modalTitle.textContent = `New message to ${recipient}`
-            modalBodyInput.value = recipient
-        })
-    }
-}
+//             modalTitle.textContent = `New message to ${recipient}`
+//             modalBodyInput.value = recipient
+//         })
+//     }
+// }
 
-function recarregarPagina() {
-    setTimeout(function() {
-        location.reload(); // recarrega a página
-    }, 4000); // 4000 milissegundos = 4 segundos
-}
+// function recarregarPagina() {
+//     setTimeout(function() {
+//         location.reload(); // recarrega a página
+//     }, 4000); // 4000 milissegundos = 4 segundos
+// }
 
-function showMessage(event){    
-    event.preventDefault(); // Para evitar o envio do formulário
+// function showMessage(event){    
+//     event.preventDefault(); // Para evitar o envio do formulário
     
-    var nome   = document.getElementById("name").value;
-    var alerta = document.getElementById("alert_success");
-    // Substitui o conteúdo usando innerHTML
-    alerta.innerHTML = "Olá "+nome+"! Sua mensagem foi enviada com sucesso.";   
+//     var nome   = document.getElementById("name").value;
+//     var alerta = document.getElementById("alert_success");
+//     // Substitui o conteúdo usando innerHTML
+//     alerta.innerHTML = "Olá "+nome+"! Sua mensagem foi enviada com sucesso.";   
 
-    document.getElementById("alert_success").removeAttribute("hidden");
-    topFunction();
+//     document.getElementById("alert_success").removeAttribute("hidden");
+//     topFunction();
 
-    // Chama a função para recarregar a página após dois segundos
-    recarregarPagina();     
-}
-
+//     // Chama a função para recarregar a página após dois segundos
+//     recarregarPagina();     
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
     const navItems = document.querySelectorAll('.nav-item');
